eb8c7b32c64352970e679f66203254453f3562fc
Richard Joy <richardjoy530@gmail.com>
2024-08-26 19:21:35 +0530

    Added client provider

--- Server/Database/IClientProvider.cs
+++ Server/Database/IClientProvider.cs
@@ -6,10 +6,6 @@ namespace Wbskt.Server.Database
     {
         ClientConenction GetClientConenctionById(int clientId);
 
-        IReadOnlyCollection<ClientConenction> GetClientConenctionsBySubcriberId(Guid channelSubcriberId);
-
-        int AddClientConnection(ClientConenction clientConenction);
-
         void InvalidateToken(int clientId);
     }
 }
--- Server/Database/Providers/ClientProvider.cs
+++ Server/Database/Providers/ClientProvider.cs
@@ -1,7 +1,6 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 using Wbskt.Server.Database;
-using Wbskt.Server.Database.Providers;
 using Wbskt.Server.Services;
 
 namespace Wbskt.Server.Providers
@@ -15,32 +14,6 @@ namespace Wbskt.Server.Providers
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _connectionString = connectionStringProvider?.ConnectionString ?? throw new ArgumentNullException(nameof(connectionStringProvider));
-        }
-
-        public int AddClientConnection(ClientConenction clientConenction)
-        {
-            if (clientConenction == null)
-                throw new ArgumentNullException(nameof(clientConenction));
-
-            using var connection = new SqlConnection(_connectionString);
-            connection.Open();
-
-            using var command = connection.CreateCommand();
-            command.CommandType = CommandType.StoredProcedure;
-            command.CommandText = "dbo.Clients_Insert";
-
-            command.Parameters.Add(new SqlParameter("@TokenId", ProviderExtensions.ReplaceDbNulls(clientConenction.TokenId)));
-            command.Parameters.Add(new SqlParameter("@Disabled", ProviderExtensions.ReplaceDbNulls(clientConenction.Disabled)));
-            command.Parameters.Add(new SqlParameter("@ClientName", ProviderExtensions.ReplaceDbNulls(clientConenction.ClientName)));
-            command.Parameters.Add(new SqlParameter("@ClientUniqueId", ProviderExtensions.ReplaceDbNulls(clientConenction.ClientUniqueId)));
-            command.Parameters.Add(new SqlParameter("@ChannelSubscriberId", ProviderExtensions.ReplaceDbNulls(clientConenction.ChannelSubscriberId)));
-
-            var id = new SqlParameter("@Id", SqlDbType.Int) { Size = int.MaxValue };
-            id.Direction = ParameterDirection.Output;
-            command.Parameters.Add(id);
-            command.ExecuteNonQuery();
-
-            return clientConenction.ClientId = (int)(ProviderExtensions.ReplaceDbNulls(id.Value) ?? 0);
         }
 
         public ClientConenction GetClientConenctionById(int clientId)
@@ -71,26 +44,6 @@ namespace Wbskt.Server.Providers
 
             command.Parameters.Add(new SqlParameter("@Id", clientId));
             command.ExecuteNonQuery();
-        }
-
-        public IReadOnlyCollection<ClientConenction> GetClientConenctionsBySubcriberId(Guid channelSubcriberId)
-        {
-            using var connection = new SqlConnection(_connectionString);
-            connection.Open();
-
-            using var command = connection.CreateCommand();
-            command.CommandType = CommandType.StoredProcedure;
-            command.CommandText = "dbo.Clients_GetBy_ChannelSubscriberId";
-
-            command.Parameters.Add(new SqlParameter("@ChannelSubscriberId", channelSubcriberId));
-
-            var result = new List<ClientConenction>();
-            using var reader = command.ExecuteReader();
-            var mapping = GetColumnMapping(reader);
-
-            while (reader.Read()) result.Add(ParseData(reader, mapping));
-
-            return result;
         }
 
         private static ClientConenction ParseData(IDataRecord reader, OrdinalColumnMapping mapping)
