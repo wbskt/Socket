c45adb275f2c588d0064a4aa9a0de67afe6fd126
Richard Joy <richardjoy530@gmail.com>
2025-05-06 21:43:46 +0530

    (chore): update version to 2.0.0.0 in installer and project files

--- Wbskt.Socket.Service/Controllers/ClientMessagesController.cs
+++ Wbskt.Socket.Service/Controllers/ClientMessagesController.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Wbskt.Common;
 using Wbskt.Common.Contracts;
